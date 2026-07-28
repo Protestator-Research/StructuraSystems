665fecda2123c98edbee1661bfbb9468401ffd11
Moritz Herzog <7609273+moritz-herzog@users.noreply.github.com>
2026-07-28 14:25:11 +0200

    Fixing Linux and Windows issues.

--- conanfile.py
+++ conanfile.py
@@ -30,11 +30,13 @@ class CppStructuraSystemsRecipe(ConanFile):
         self.requires("boost/[>=1.86.0 <2]")
         self.requires("libcurl/[>=8.4.0 <9]")
         self.requires("nlohmann_json/[>=3.11.3 <3.13]")
-        self.requires("qt/6.11.1")
         self.requires("md4c/0.5.2")
         self.requires("sysmllib/2607beta")
         self.requires("yaml-cpp/0.8.0")
         self.requires("openssl/3.6.3")
+
+        if self.settings.os == "Linux":
+            self.requires("qt/6.11.1")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -51,7 +53,10 @@ class CppStructuraSystemsRecipe(ConanFile):
             self.options["yaml-cpp/*"].shared=True
             self.options["md4c/*"].shared=True
             self.options["openssl/*"].shared=True
-            self.options["qt/*"].shared = True
+
+            if self.settings.os == "Linux":
+                self.options["qt/*"].shared = True
+
         else:
             self.options["boost/*"].shared = False
             self.options["libcurl/*"].shared = False
@@ -60,11 +65,11 @@ class CppStructuraSystemsRecipe(ConanFile):
             self.options["yaml-cpp/*"].shared=False
             self.options["md4c/*"].shared=False
             self.options["openssl/*"].shared=False
-            self.options["qt/*"].shared = False
-
-        self.options["qt/*"].qtcharts = True
-        #self.options["qt/*"].qthttpserver = True
-
+            if self.settings.os == "Linux":
+                self.options["qt/*"].shared = False
+        
+        if self.settings.os == "Linux"
+            self.options["qt/*"].qtcharts = True
     
     def layout(self):
         cmake_layout(self)
@@ -84,7 +89,9 @@ class CppStructuraSystemsRecipe(ConanFile):
         cmake.build()
 
     def build_requirements(self):
-        self.tool_requires("qt/6.11.1")
+        if self.settings.os == "Linux":
+            self.tool_requires("qt/6.11.1")
+        
         self.tool_requires("cmake/[>=3.30.0 <5]")
 
 
